a0d44c64e074a2f192ddce337f511646c00f37d0
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2024-09-17 23:00:34 +0300

    feat(thinkpad): update config to account for thinkpad

--- scripts/.local/bin/scripts/monitors.sh
+++ scripts/.local/bin/scripts/monitors.sh
@@ -1,1 +1,10 @@
-xrandr --output HDMI-1 --auto --primary --left-of eDP-1 
+#!/bin/bash
+
+# Check if HDMI-1 is connected
+if xrandr | grep -q "HDMI-1 connected"; then
+    # Set HDMI-1 as primary and to the left of eDP-1
+    xrandr --output HDMI-1 --auto --primary --left-of eDP-1
+else
+    # Set eDP-1 as the primary display
+    xrandr --output eDP-1 --auto --primary
+fi
