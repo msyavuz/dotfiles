99da135dd9bc51c101352588b957a58ff148b6d8
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2024-09-24 10:10:44 +0300

    feat(picom): add picom and transparency

--- scripts/.local/bin/scripts/monitors.sh
+++ scripts/.local/bin/scripts/monitors.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if HDMI-1 is connected
-if xrandr | grep -q "HDMI-1 connected"; then
+if xrandr | grep "HDMI-1 connected"; then
     # Set HDMI-1 as primary and to the left of eDP-1
     xrandr --output HDMI-1 --auto --primary --left-of eDP-1
 else
